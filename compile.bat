d77a1f1bd7e2ec1fda9fef5d9c511962330a74cb
PaoDerDoktor <paoderdoktor@gmail.com>
2020-06-24 11:43:33 +0200

    Ajouté la compilation des fichiers .html complets

--- compile.bat
+++ compile.bat
@@ -7,5 +7,13 @@ for /d %%i in (day*) do (
     cd %%i
     pandoc -t html -o part1.phtml part1.md
     pandoc -t html -o part2.phtml part2.md
+    del "%%i".html >NUL 2>&1
+    copy /y NUL "%%i".html >NUL
+    echo ^<section id='translation_part_one'^> > "%%i".html
+    copy "%%i".html+part1.phtml "%%i".html >NUL
+    echo ^</section^> >> "%%i".html
+    echo ^<section id='translation_part_two'^> >> "%%i".html
+    copy "%%i".html+part2.phtml "%%i".html >NUL
+    echo ^</section^> >> "%%i".html
     cd ..
 )
