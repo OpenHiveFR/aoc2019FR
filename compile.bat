674681bcd954bc30d1dd685cc331cac15c7a786c
PaoDerDoktor <paoderdoktor@gmail.com>
2020-06-29 11:09:55 +0200

    Changé le compilateur pour inclure le CSS

--- compile.bat
+++ compile.bat
@@ -9,11 +9,19 @@ for /d %%i in (day*) do (
     pandoc -t html -o part2.phtml part2.md
     del "%%i".html >NUL 2>&1
     copy /y NUL "%%i".html >NUL
-    echo ^<section id='translation_part_one'^> > "%%i".html
+    echo ^<!DOCTYPE html^> >> "%%i".html
+    echo ^<html lang='fr'^> >> "%%i".html
+    echo ^<head^> >> "%%i".html
+    echo ^<link rel='stylesheet' type='text/css' href='../../../../skins/sc2020-translation.css'^> >> "%%i".html
+    echo ^</head^> >> "%%i".html
+    echo ^<body^> >> "%%i".html
+    echo ^<section id='translation_part_one'^> >> "%%i".html
     copy "%%i".html+part1.phtml "%%i".html >NUL
     echo ^</section^> >> "%%i".html
     echo ^<section id='translation_part_two'^> >> "%%i".html
     copy "%%i".html+part2.phtml "%%i".html >NUL
     echo ^</section^> >> "%%i".html
+    echo ^</body^> >> "%%i".html
+    echo ^</html^> >> "%%i".html
     cd ..
 )
